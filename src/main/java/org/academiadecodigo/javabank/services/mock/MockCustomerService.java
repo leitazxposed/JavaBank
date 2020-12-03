425514bc90faabb9484d8b4db42ac66db85184d1
Paulo Resende <paulo.resende@academiadecodigo.org>
2020-12-03 15:21:40 +0000

    added javabank step10 springMVC

---
+++ src/main/java/org/academiadecodigo/javabank/services/mock/MockCustomerService.java
@@ -0,0 +1,69 @@
+package org.academiadecodigo.javabank.services.mock;
+
+import org.academiadecodigo.javabank.persistence.model.AbstractModel;
+import org.academiadecodigo.javabank.persistence.model.Customer;
+import org.academiadecodigo.javabank.persistence.model.Recipient;
+import org.academiadecodigo.javabank.persistence.model.account.Account;
+import org.academiadecodigo.javabank.services.CustomerService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * A mock {@link CustomerService} implementation
+ */
+public class MockCustomerService extends AbstractMockService<Customer> implements CustomerService {
+
+    /**
+     * @see CustomerService#get(Integer)
+     */
+    @Override
+    public Customer get(Integer id) {
+        return modelMap.get(id);
+    }
+
+    /**
+     * @see CustomerService#getBalance(Integer)
+     */
+    @Override
+    public double getBalance(Integer customerId) {
+
+        List<Account> accounts = modelMap.get(customerId).getAccounts();
+
+        return accounts.stream()
+                .mapToDouble(Account::getBalance)
+                .sum();
+    }
+
+    /**
+     * @see CustomerService#list()
+     */
+    @Override
+    public List<Customer> list() {
+        return new ArrayList<>(modelMap.values());
+    }
+
+    /**
+     * @see CustomerService#listCustomerAccountIds(Integer)
+     */
+    @Override
+    public Set<Integer> listCustomerAccountIds(Integer id) {
+
+        List<Account> accounts = modelMap.get(id).getAccounts();
+
+        return accounts.stream()
+                .map(AbstractModel::getId)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * @see CustomerService#listRecipients(Integer)
+     */
+    @Override
+    public List<Recipient> listRecipients(Integer id) {
+        return modelMap.get(id).getRecipients();
+    }
+}
